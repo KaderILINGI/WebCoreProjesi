9500d5078f273452b4f17f2d203240349c500508
SABAH YAZILIM <SABAH YAZILIM@203-EMRAH>
2023-05-25 14:58:22 +0300

    Add project files.

---
+++ WebCoreProjesi/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCoreProjesi.Models;
+
+namespace WebCoreProjesi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+            
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+
+        public IActionResult Out()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ WebCoreProjesi/Models/Entitys/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCoreProjesi.Models.Entitys
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [StringLength(30)]
+        public string? Name { get; set; }
+
+
+        [StringLength(30)]
+        public string? Surname { get; set; } = null;
+
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+
+        [Required]
+        [StringLength(30)]
+        public string? UserName { get; set; }
+
+
+        [Required]
+        [StringLength(250)]
+        public string Password { get; set; }    
+
+        public bool Activate { get; set; }
+        public DateTime Tarih { get; set; }
+
+    }
+}
